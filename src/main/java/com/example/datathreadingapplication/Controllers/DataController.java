c20d6d3dc1b9187a961e5d34fd11d1e3b328ab94
AinisALaur <ainislaurinavicius@gmail.com>
2025-12-03 17:36:47 +0200

    Created skeletons for fxml files, added a button to select a file to read data from inside of the DataController clas

--- src/main/java/com/example/datathreadingapplication/Controllers/DataController.java
+++ src/main/java/com/example/datathreadingapplication/Controllers/DataController.java
@@ -1,14 +1,29 @@
 package com.example.datathreadingapplication.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class DataController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField selectedFilePath;
+
+    @FXML
+    VBox root;
+
+    public void onFileSelect() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(root.getScene().getWindow());
+
+        if (selectedFile != null) {
+            selectedFilePath.setText(selectedFile.getAbsolutePath());
+        }
     }
 }
---
+++ src/main/java/com/example/datathreadingapplication/Controllers/TableViewController.java
@@ -0,0 +1,4 @@
+package com.example.datathreadingapplication.Controllers;
+
+public class TableViewController {
+}
